c961ef71736ac372f6cb02b5a0349fd5c5b04297
Seto El Kahfi <setoelkahfi@gmail.com>
2015-10-23 16:33:03 +0700

    Polyfill for supporting Object.create on old browser

--- Section 6 - Building Objects/Lecture 63 - Object.create and Pure Prototypal Inheritance/app.js
+++ Section 6 - Building Objects/Lecture 63 - Object.create and Pure Prototypal Inheritance/app.js
@@ -1,9 +1,24 @@
-String.prototype.isLengthGreaterThan = function(limit) {
-    return this.length > limit;
+// Polyfill: code that adds a feature which the engine may lack.
+if (!Object.create) {
+    Object.create = function(o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation only accepts the first parameter.');   
+        }
+        function F() {};
+        F.prototype = o;
+        return new F();
+    };
 }
 
-console.log("John".isLengthGreaterThan(3));
-
-Number.prototype.isPositive = function() {
-    return this   
+var person = {
+    firstName: 'Default',
+    lastName: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstName;
+    }
 }
+
+var john = Object.create(person);
+john.firstName = 'John';
+john.lastName = 'Doe';
+console.log(john);
